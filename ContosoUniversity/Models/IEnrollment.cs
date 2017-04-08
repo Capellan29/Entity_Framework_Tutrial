4056ed5e53ed3b0ab65084489db90298f1a3a978
Capellán <jorlincap@gmail.com>
2017-04-07 22:54:26 -0400

    Second Tutorial - Implementing Basic CRUD Functionality

---
+++ ContosoUniversity/Models/IEnrollment.cs
@@ -0,0 +1,12 @@
+﻿namespace ContosoUniversity.Models
+{
+    public interface IEnrollment
+    {
+        Course Course { get; set; }
+        int CourseID { get; set; }
+        int EnrollmentID { get; set; }
+        Grade? Grade { get; set; }
+        Student Student { get; set; }
+        int StudentID { get; set; }
+    }
+}
